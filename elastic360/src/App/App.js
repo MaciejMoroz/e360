cb34fb7cb19c292b306991465ad03ca6690fccc7
Maciej Moroz <maciej.moroz89@gmail.com>
2019-07-18 19:28:57 +0200

    init

---
+++ elastic360/src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+
+function App() {
+  return (
+    <component className="component">
+      <div className="wrapper">
+      </div>
+    </component>
+  );
+}
+
+export default App;
